b9332ccad0355cbabcb87ef81d5a2be403b0e9e8
Fara Akhatova <faraakhatova@gmail.com>
2021-12-14 11:23:06 -0700

    Add modification for new user api endpoint

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express')
 const app = express();
+// const cors = require('cors');
+// const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000;
 
-
+// app.use(cors());
 app.use(express.json());// midleware to apply for request 
-
-app.locals.players = [
-    {
-    "id": 1,
-    "name": "Fara",
-    "wins": 0,
-    "losts": 0
-}
+// app.use(bodyParser.urlencoded({ extended: false }));
+const players = [
+    // {
+    //     id:
+    //     userName,
+    //     password,
+    //     wins, 
+    //     losts,
+    // }
 ]
+app.locals = {
+    title: 'Rock-Paper-Scissors API',
+    players,
+}
 
 
 
@@ -20,17 +27,19 @@ app.listen(
     PORT, () => console.log(`App listening on http://localhost:${PORT}`)
 );
 
-app.get('/', (req,res) => {
-    res.send('Hello, and welcome... Please add endpoint /players  - to your URL to be able request data from this API')
-})
+// app.get('/', (req,res) => {
+//     res.send('Hello, and welcome... Please add endpoint /players  - to your URL to be able request data from this API')
+// })
 
 app.get('/players', (req, res) => {
-        res.status(200).send(app.locals.players);
+    const { players } = app.locals;
+        res.status(200).send( {players} );
 });
 
 app.get('/players/:id', (req, res) => {
     const { id }  = req.params;
-    const match = app.locals.players.filter(player => player.id == id);
+    const { players } = app.locals;
+    const match = players.filter(player => player.id == id);
 
     if (!match) return res.status(404).send({message: `No player found with an id of ${id}`});
 
@@ -38,8 +47,27 @@ app.get('/players/:id', (req, res) => {
 });
 
 app.post('/players/new', (req, res) => {
-    const newPlay  = req.body;
-    
+    const { userName, password, wins, losts }= req.body;
+    const requiredProperties = ['userName', 'password', 'wins', 'losts'];
+    const newId = String(Date.now());
+
+    for (let requiredParameter of requiredProperties) {
+        if (req.body[requiredParameter] === undefined) {
+        return res.status(422).json({
+            message: `You are missing a required parameter of ${requiredParameter}`
+        });
+        }
+    }
+
+    const newPlay = {
+        id : newId,
+        userName,
+        password,
+        wins,
+        losts,
+    }
+
+
     const match = app.locals.players.find(player => {
 
     if(player.id === newPlay.id) {
@@ -48,8 +76,7 @@ app.post('/players/new', (req, res) => {
     
     })
 
-    app.locals.players = [...app.locals.players, newPlay]
-
+    
     res.status(201).send(newPlay)
 });
 
